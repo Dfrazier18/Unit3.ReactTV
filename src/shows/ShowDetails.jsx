598a39005f792cadaa47a89bf565f8300704642d
Devin Frazier <Dev.fraz18@gmail.com>
2025-03-10 18:58:43 -0400

    finished App, ShowSelection & ShowDetails

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,34 @@
 import "./shows.css";
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  if (!show) {
+    return (
+      <section className="show-details">
+        <h2>Show Details</h2>
+        <p>Select a show to learn more.</p>
+      </section>
+    );
+  }
+
+  return (
+    <div className="show-details">
+      <h2>{show.name}</h2>
+      <p>
+        <strong>Genre:</strong> {show.genre}
+      </p>
+      <EpisodeList
+        name="Episodes"
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
